ead64c3bd7673eb26e9f0ef3a352101d39f724c2
daradeb022@student.wethinkcode.co.za <daradeb022@student.wethinkcode.co.za>
2023-05-23 16:09:00 +0200

    changed mapbox api to google maps api

---
+++ defaultMap/script.js
@@ -0,0 +1,7 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'),{
+        center: {lat: 29.8574, lng: -31.0216},
+        zoom: 8,
+        mapId: '689f9b3e6cf23043'
+    });
+}
